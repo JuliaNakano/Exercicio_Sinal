04f890f81c9c21ff6ba0471bac6496aae7e7f4ec
JuliaNakano <jumartinsnakano@gmail.com>
2021-11-05 21:22:55 -0300

    Solução do Exercício Sinal

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio_Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //exibir se o numero é negativo, positivo ou zero
+            double numerodigitado;
+            
+            Console.WriteLine("\nO programa consiste em analisar o numero digitado e dizer se é negaivo, positivo ou igual à zero.");
+            Console.WriteLine("Para iniciá-lo basta....");
+            
+            Console.Write("\nDigite um número: ");
+            numerodigitado = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            if(numerodigitado>0)
+            {
+                Console.WriteLine("O número digitado é positivo.");
+                
+            }
+            if(numerodigitado==0)
+            {
+                Console.WriteLine("O número digitado é igual a zero");
+            }
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if(numerodigitado<0)
+            {
+                Console.WriteLine("O número digitado é negativo");
+                
+            }
+           
+           Console.ResetColor();
+            Console.WriteLine("");
+
+
+
+        }
+    }
+}
